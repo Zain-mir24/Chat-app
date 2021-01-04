cf618b1730f2e45953e7ca04dc74417a8a18a0df
Zain Mir <zainnaeemk10@gmail.com>
2021-01-04 18:51:06 +0500

    Chat-app secong commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,7 +6,6 @@ socket.on('message',(message)=>{
 // socket.on('countupdated',(count)=>{
 //   console.log('The count has been updated!',count)
 // })
-document.querySelector('#increment').addEventListener('click',()=>{
-    console.log('clicked')
-    socket.emit('increment')
+document.querySelector('#message-form').addEventListener('submit',(e)=>{
+  e.preventDefault
 })
--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const port= process.env.PORT|| 3000
 
 const publicDirectoryPath = path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath)) 
-let count = 0
+let message="";
 
  io.on('connection',(socket)=>{
      console.log('New websocket connection')
-     socket.emit('message','welcome')
+    //  socket.emit('message','welcome')
     //  socket.emit('countupdated',count)
-    //  socket.on('increment',()=>{
-    //      count++
-    //      socket.emit('countupdated',count)
-    //  })
+     socket.on('Message',()=>{
+        message="A new user has been added"
+         socket.emit('message',message)
+     })
  })
 
 server.listen(port,()=>{
